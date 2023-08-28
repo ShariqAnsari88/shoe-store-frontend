8434e7b12a3c8c90370eb8e30d763320a0eb24bc
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-08-28 17:57:57 +0300

    Update banner style

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -16,7 +16,7 @@ export default function Banner() {
         aria-hidden="true"
       >
         <div
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-neonGreen to-offWhite opacity-30"
           style={{
             clipPath:
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
@@ -28,7 +28,7 @@ export default function Banner() {
         aria-hidden="true"
       >
         <div
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-neonGreen to-offWhite opacity-30"
           style={{
             clipPath:
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
@@ -59,11 +59,11 @@ export default function Banner() {
             <circle cx={1} cy={1} r={1} />
           </svg>
           <p className="text-sm leading-6 text-gray-900">
-            Безплатна доставка при
+            Безплатна доставка при покука над
           </p>
         </div>
         <div className="cursor-pointer flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
-          Покупка над <span aria-hidden="true">50лв</span>
+          50лв
         </div>
       </div>
       <div className="flex flex-1 justify-end">
