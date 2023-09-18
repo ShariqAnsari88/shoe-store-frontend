8c4be5afc9f4bfea4788f2411d3e0d732cc56c47
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-09-18 18:03:54 +0300

    Build localePath for vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next-i18next').UserConfig} */
-// const path = require("path");
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "bg",
     locales: ["en", "bg", "it"],
+    localePath: path.resolve('./public/locales'),
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
   },
 };
