b4e3ccadc67abeae73b7d4280ae940473a943582
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-10-10 00:02:37 +0300

    Add selectedImage check

--- components/ProductDetailsCarousel.jsx
+++ components/ProductDetailsCarousel.jsx
@@ -3,15 +3,19 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import Image from "next/image";
 
 const ProductDetailsCarousel = ({ image, sliderImages, selected }) => {
+  const selectedImage = sliderImages
+    ? sliderImages.find((a) => a.attributes.hash === selected)?.attributes
+    : null;
+
   return (
     <div className="text-[20px] text-neonGreenLighter w-full max-w-[1360px] mx-auto sticky top-[50px]">
       <Image
         className="rounded-md"
         width={800}
         height={600}
-        key={image.id}
-        src={image.attributes.url}
-        alt={image.attributes.name}
+        key={selectedImage?.id ?? image.id}
+        src={selectedImage?.url ?? image.attributes.url}
+        alt={selectedImage?.name ?? image.attributes.name}
       />
     </div>
   );
