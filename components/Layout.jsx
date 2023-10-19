0982c0a0595ca6369f7f85e105858505652cfb50
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-10-19 15:04:50 +0300

    Add vercel analytics package

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function Layout(props) {
   const Component = props.component;
   return (
+    <>
       <Component {...props.pageProps} />
+      <Analytics />
+    </>
   );
 }
 
