64ee0d536fe4796daff9d191381ef14ad4875c1b
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-10-29 15:57:26 +0200

    Fix pagination articles

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -17,7 +17,7 @@ const Category = ({ category, products, slug }) => {
   }, [query]);
 
   const { data, error, isLoading } = useSWR(
-    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=${pageIndex}&pagination[pageSize]=${maxResult}&locale=${locale}`,
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&locale=${locale}`,
     fetchDataFromApi,
     {
       fallbackData: products,
@@ -26,9 +26,9 @@ const Category = ({ category, products, slug }) => {
 
   return (
     <Container>
-      <div className="w-full md:py-20 relative h-screen">
+      <div className="w-full md:py-20 relative">
         <Wrapper>
-          <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+          <div className="text-center max-w-[800px]mx-auto mt-8 md:mt-0">
             <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
               {category?.data?.[0]?.attributes?.name}
             </div>
@@ -43,7 +43,7 @@ const Category = ({ category, products, slug }) => {
           {/* products grid end */}
 
           {/* PAGINATION BUTTONS START */}
-          {data?.meta?.pagination?.total > maxResult && (
+          {/* {data?.meta?.pagination?.total > maxResult && (
             <div className="flex gap-3 items-center justify-center my-16 md:my-0">
               <button
                 className={`rounded py-2 px-4 bg-#393646 text-offWhite disabled:bg-gray-200 disabled:text-gray-500`}
@@ -67,7 +67,7 @@ const Category = ({ category, products, slug }) => {
                 Next
               </button>
             </div>
-          )}
+          )} */}
           {/* PAGINATION BUTTONS END */}
           {isLoading && (
             <div className="absolute top-0 left-0 w-full h-full bg-white/[0.5] flex flex-col gap-5 justify-center items-center">
@@ -85,10 +85,10 @@ export default Category;
 
 export async function getServerSideProps({ params: { slug }, locale }) {
   const category = await fetchDataFromApi(
-    `/api/categories?filters[slug][$eq]=${slug}&locale=${locale}`
+    `/api/categories?populate=*&filters[slug][$eq]=${slug}&locale=${locale}`
   );
   const products = await fetchDataFromApi(
-    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}&locale=${locale}`
+    `/api/products?populate=*&filters[categories][slug][$eq]=${slug}&locale=${locale}`
   );
 
   return {
