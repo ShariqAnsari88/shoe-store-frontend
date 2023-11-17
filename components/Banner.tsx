a82190a29db7e0bf5e398ca0f97be1eacf0cea97
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-11-17 21:30:24 +0200

    Fix banner styles

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,6 @@ import useCurrency from '@/hooks/useCurrency'
 import { useAppSelector } from '@/store/hooks'
 import { selectShowBanner, setShowBanner } from '@/store/uiSlice'
 
-
 export default function Banner() {
 	const dispatch = useAppDispatch()
 	const shouldShowBanner = useAppSelector(selectShowBanner)
@@ -16,40 +15,18 @@ export default function Banner() {
 	if (!shouldShowBanner) return null
 
 	return (
-		<div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-			<div
-				className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-				aria-hidden="true"
-			>
-				<div
-					className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-neonGreen to-offWhite opacity-30"
-					style={{
-						clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
-					}}
-				/>
-			</div>
-			<div
-				className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-				aria-hidden="true"
-			>
-				<div
-					className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-neonGreen to-offWhite opacity-30"
-					style={{
-						clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
-					}}
-				/>
-			</div>
-			<div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-				<div className="flex flex-row">
+		<div className="relative isolate flex  sm:flex-row justify-between items-center gap-2 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5">
+			<div className="w-6 h-6 md:w-12 md:h-12" />
+
+			<div className="flex flex-wrap gap-x-4 gap-y-2 items-center">
+				<div className="flex justify-center items-center  sm:flex-row gap-2">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
 						strokeWidth={1.5}
 						stroke="#181516"
-						className="w-6 h-6"
+						className="sm:w-8 w-5"
 					>
 						<path
 							strokeLinecap="round"
@@ -64,21 +41,22 @@ export default function Banner() {
 					>
 						<circle cx={1} cy={1} r={1} />
 					</svg>
-					<p className="text-sm md:text-lg leading-6 text-gray-900 font-semibold uppercase">{t('description')}</p>
-				</div>
-				<div className="uppercase cursor-pointer font-semibold flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm md:text-lg font-phlatt-9000 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
-          50 {currency}
+					<p className="text-sm md:text-lg leading-6 text-gray-900 font-semibold uppercase">
+						{t('description')}
+					</p>
+
+					<div className="uppercase ml-4 cursor-pointer font-semibold flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm md:text-lg font-phlatt-9000 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
+            50 {currency}
+					</div>
 				</div>
 			</div>
-			<div className="flex flex-1 justify-end">
-				<button
-					onClick={() => dispatch(setShowBanner(false))}
-					type="button"
-					className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
-				>
-					<XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
-				</button>
-			</div>
+			<button
+				onClick={() => dispatch(setShowBanner(false))}
+				type="button"
+				className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+			>
+				<XMarkIcon className="sm:w-7 w-6 text-gray-900" aria-hidden="true" />
+			</button>
 		</div>
 	)
 }
